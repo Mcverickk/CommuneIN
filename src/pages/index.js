87bab9aea548768f3d16df80365884fca2634b9e
chiragpay3 <chiraga@pay3.money>
2023-12-09 18:29:38 +0530

    refactored loginmodal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,14 @@
 import { useEffect, useState } from "react";
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import AadharLogin from "../components/AadharLogin";
-import PasskeyLogin from "../components/PasskeyLogin";
+import LoginModal from "../components/LoginModal";
 
 
 // Use the Country Identity hook to get the status of the user.
 export default function Home() {
-  const {LogInWithAnonAadhaar, aadhaarVerified} = AadharLogin();
-  const {RegisterPasskeyButton, VerifyPasskeyButton, verifyPasskey, passkeyRegistered, passkeyVerified} = PasskeyLogin();
 
   return (
     <>
-      <div>
-        <ConnectButton />
-        <LogInWithAnonAadhaar />
-      </div>
-      {aadhaarVerified && <RegisterPasskeyButton/>}
-      <br />
-      {passkeyRegistered && <VerifyPasskeyButton/>}
-      <br/>
-      {passkeyVerified && <p>Verified Passkey ✅</p>}
+      <LoginModal/>
     </>
   );
 }
