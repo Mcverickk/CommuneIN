a10a62670d061cd9ef3027969a1a8acb8573afbc
chiragpay3 <chiraga@pay3.money>
2023-12-10 03:25:24 +0530

    merge changes

--- src/pages/launchpad.js
+++ src/pages/launchpad.js
@@ -12,8 +12,8 @@ export default function Launchpad() {
        
         <NftComponent pic="./communein.jpeg" info="NFTs for the exclusive Indian community." name="CommuneIN" price="0.01 mMATIC" timeLeft="3 hrs"/>
         <div className="listErc">
-        <ErcComponent/>
-        <ErcComponent/>
+        <ErcComponent name="Token 1" info="NFTs for the exclusive Indian community." sold="$1030" totalSupply="$40000" timeLeft="2 days"/>
+        <ErcComponent name="Token 2" info="NFTs for the exclusive Indian community." sold="$2230" totalSupply="$5000" timeLeft="1 day"/>
         </div>
         <NftComponent pic="./mrgadget.jpeg" info="A fun retro gadget collection." name="Mr.Gadget" price="2 mMATIC" timeLeft="3 days" disabled= {true}/>
         </div>
